f58cf546c0126bddd1d13e336541e71c4a45b4f2
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2023-04-11 14:15:48 -0700

    feat: render two triangles with different fragment shaders

--- src/main.cpp
+++ src/main.cpp
@@ -17,6 +17,13 @@ const char *fragmentShaderSource = "#version 330 core\n"
     "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
     "}\n\0";
 
+const char *fragmentShaderSource2 = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 1.0f, 0.0f, 1.0f);\n"
+    "}\n\0";
+
 /* glfw callback function, resize window */
 void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
@@ -88,13 +95,18 @@ int main() {
   }
 
   // compile fragment shader
-  unsigned int fragmentShader;
+  unsigned int fragmentShader, fragmentShader2;
   fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
   glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
   glCompileShader(fragmentShader);
 
-  unsigned int shaderProgram;
+  fragmentShader2 = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader2, 1, &fragmentShaderSource2, NULL);
+  glCompileShader(fragmentShader2);
+
+  unsigned int shaderProgram, shaderProgram2;
   shaderProgram = glCreateProgram(); // initialize shader program
+  shaderProgram2 = glCreateProgram();
 
   // link shaders
   glAttachShader(shaderProgram, vertexShader);
@@ -104,34 +116,56 @@ int main() {
   glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
   if(!success) {
     glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-    std::cout << "ERROR::HADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+  }
+
+  glAttachShader(shaderProgram2, vertexShader);
+  glAttachShader(shaderProgram2, fragmentShader2);
+  glLinkProgram(shaderProgram2);
+
+  glGetProgramiv(shaderProgram2, GL_LINK_STATUS, &success);
+  if(!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
   }
 
   // delete shader objects after linking
   glDeleteShader(vertexShader);
   glDeleteShader(fragmentShader);
-
+  glDeleteShader(fragmentShader2);
 
   // BUFFERS
-  float vertices[] = {
-  -0.5f, -0.5f, 0.0f,
-   0.5f, -0.5f, 0.0f,
-   0.0f,  0.5f, 0.0f
+  float t1[] = {
+  -1.0f, -0.5f, 0.0f,
+   0.0f, -0.5f, 0.0f,
+  -0.5f,  0.5f, 0.0f,
   };
 
-  unsigned int VAO;
-  glGenVertexArrays(1, &VAO); 
-  glBindVertexArray(VAO);
+  float t2[] = {
+   1.0f, -0.5f, 0.0f,
+   0.0f, -0.5f, 0.0f,
+   0.5f,  0.5f, 0.0f 
+  };
 
-  unsigned int VBO;
-  glGenBuffers(1, &VBO); // create 1 buffer with ID stored in VBO
-  glBindBuffer(GL_ARRAY_BUFFER, VBO); // set buffer type
+  unsigned int VAO[2], VBO[2];
+
+  glGenVertexArrays(2, VAO); 
+  glGenBuffers(2, VBO); // create 2 buffer with ID stored in VBO
+
+  glBindVertexArray(VAO[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]); // set buffer type
+  glBufferData(GL_ARRAY_BUFFER, sizeof(t1), t1, GL_STATIC_DRAW);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(VAO[1]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[1]); // set buffer type
 
   /* copy data to buffer:
    * GL_STREAM_DRAW, GL_STATIC_DRAW,GL_DYNAMIC_DRAW specifies usage pattern
    * use GL_STATIC_DRAW since we are drawing a static triangle that does
    * not change between frames */
-  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(t2), t2, GL_STATIC_DRAW);
 
   /* tell opengl how to interpret vertex data:
    * 0: configure vertex attribute for location 0
@@ -152,9 +186,13 @@ int main() {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-
     glUseProgram(shaderProgram); // use the compiled shader program
-    glBindVertexArray(VAO);
+
+    glBindVertexArray(VAO[0]);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glUseProgram(shaderProgram2); // use the compiled shader program
+    glBindVertexArray(VAO[1]);
     glDrawArrays(GL_TRIANGLES, 0, 3);
 
     /* double buffer: front and back buffer, prevent flickering by
